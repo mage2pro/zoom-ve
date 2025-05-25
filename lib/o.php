a4c598062b83b65a51237f77573211020939deb8
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-25 12:04:49 +0100

    https://github.com/mage2pro/zoom-ve/issues/3

--- lib/o.php
+++ lib/o.php
@@ -5,6 +5,7 @@ use Dfe\ZoomVe\Helper\Config as C;
  * 2025-05-25
  * @used-by \Dfe\ZoomVe\Model\Config\Source\Generic::toOptionArray()
  * @used-by \Dfe\ZoomVe\Model\Config\Source\OriginCity::toOptionArray()
+ * @used-by \Dfe\ZoomVe\Model\Config\Source\OriginShipment::toOptionArray()
  * @used-by \Dfe\ZoomVe\Model\Config\Source\Unitofmeasure::toOptionArray()
  */
 function dfe_zv_cfg() {return df_o(C::class);}
