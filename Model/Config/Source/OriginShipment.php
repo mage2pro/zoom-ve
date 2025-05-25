2d849866059f26c9e3cc7c6de1a9ab693f3cfd7c
Dmitrii Fediuk <dfediuk@gmail.com>
2025-05-25 12:04:38 +0100

    https://github.com/mage2pro/zoom-ve/issues/3

--- Model/Config/Source/OriginShipment.php
+++ Model/Config/Source/OriginShipment.php
@@ -15,7 +15,7 @@ class OriginShipment extends \Dfe\ZoomVe\Model\Config\Source\Generic
 	 */
 	function toOptionArray()
 	{
-		$orShipArr = $this->carrierConfig->getCode($this->_code);
+		$orShipArr = dfe_zv_cfg()->getCode($this->_code);
 		$returnArr = [];
 		foreach ($orShipArr as $key => $val) {
 			$returnArr[] = ['value' => $key, 'label' => $key];
